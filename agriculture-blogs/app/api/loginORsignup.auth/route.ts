2bccacc9a5ffb43b9a74b13d5f9ed44ebc1b2ca7
Uznain Rashid <147235060+uznainrashid@users.noreply.github.com>
2026-01-06 15:02:10 +0500

    cnnection done

---
+++ agriculture-blogs/app/api/loginORsignup.auth/route.ts
@@ -0,0 +1,45 @@
+import ConnectDB from "@/lib/db";
+
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@/models/user";
+
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+    }
+
+    await ConnectDB();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json({ message: "User already exists" }, { status: 409 });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(
+      { 
+        success: true,
+        message: "User registered successfully",
+      newUser,
+      },
+      { status: 201 }
+    );
+
+  } catch (error: any) {
+    console.error("Error:", error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
